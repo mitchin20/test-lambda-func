a47921ed276f60544df566eedff56c5a082046ba
Mit Chin <mitchin10@gmail.com>
2024-10-02 13:10:02 -0400

    Update lambda function

--- src/app/lambda_function.py
+++ src/app/lambda_function.py
@@ -2,9 +2,15 @@ import json
 from services.random_number_service import generate_random_number
 
 def lambda_handler(event, context):
-    number = generate_random_number()
-    message = f"Random number generated: {number}"
-    return {
-        'statusCode': 200,
-        'body': json.dumps(message)
-    }
+    try:
+        number = generate_random_number()
+        message = f"Random number generated: {number}"
+        return {
+            'statusCode': 200,
+            'body': json.dumps(message)
+        }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps(str(e))
+        }
